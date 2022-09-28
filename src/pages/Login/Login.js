1beb5f380f48429bf3be60c126baac962c43be84
Gustavo <gustavomedeiros15@outlook.com.br>
2022-09-28 14:06:01 -0300

    Commit 01
    Tela inicial e de login

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,95 @@
+
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import * as Animatable from 'react-native-animatable'
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+        <Animatable.View animation='fadeInLeft' delay={500} style={styles.containerHeader}> 
+            <Text style={styles.message}>Bem-vindo, aventureiro(a)!</Text>
+        </Animatable.View> 
+
+        <Animatable.View animation='fadeInUp' style={styles.containerForm}>
+            <Text style={styles.title}>Email</Text>
+            <TextInput 
+            placeholder='Digite um Email...'
+            style={styles.input}
+            />
+
+            <Text style={styles.title}>Senha</Text>
+            <TextInput 
+            placeholder='Sua senha...'
+            style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Acessar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonRegister}>
+                <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+            </TouchableOpacity>
+        </Animatable.View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#rgb(32,33,45);"
+    },
+    containerHeader: {
+        marginTop: "14%",
+        marginBottom: "8%",
+        paddingStart: "5%"
+    },
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: "#fff"
+    },
+    containerForm: {
+        backgroundColor: "#fff",
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: "5%",
+        paddingEnd: "5%"
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 28,
+    },
+    input: {
+        borderBottomWidth: 1,
+        height: 40,
+        marginBotton: 12,
+        fontSize: 16
+    },
+    button: {
+        backgroundColor: "#rgb(32,33,45);",
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    buttonRegister: {
+        marginTop: 14,
+        alignSelf: 'center'
+    },
+    registerText: {
+        color: "#a1a1a1"
+    }
+})
+
+export default Login
---
+++ src/pages/Welcome/Welcome.js
@@ -0,0 +1,84 @@
+
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import * as Animatable from 'react-native-animatable'
+
+import { useNavigation } from '@react-navigation/native'
+
+const Welcome = () => {
+    const navigation = useNavigation()
+  return (
+    <View style={styles.container}>
+        <View style={styles.containerLogo}>
+            <Animatable.Image 
+            animation="flipInY"
+            source={require('../../assets/ring.png')}
+            style={{width: '80%'}}
+            risizeMode='contain'
+            />
+        </View>
+        <Animatable.View delay={600} animation="fadeInUp"style={styles.containerForm}>
+            <Text style={styles.title}>Entre na Terra Média e descubra suas maravilhas que vão aquecer seu coração.</Text>
+            <Text style={styles.text}>Faça o login para começar</Text>
+
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={ ()=> navigation.navigate('Login')}
+            >
+                <Text style={styles.buttonText}>Acessar</Text>
+            </TouchableOpacity>
+        </Animatable.View>
+
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"#rgb(32,33,45);",
+    },
+    containerLogo:{
+        flex:2,
+        backgroundColor:"#rgb(32,33,45);",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    containerForm:{
+        flex:1.3,
+        backgroundColor: "#fff",
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12,
+    },
+    text: {
+        color: "#a1a1a1"
+    },
+    button: {
+        position: "absolute",
+        backgroundColor: '#rgb(32,33,45);',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: "18%",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: 'bold',
+    }
+})
+
+export default Welcome
